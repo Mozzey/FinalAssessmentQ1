8279ca337122010a855f99d67b91c33dc69ab9e4
Mozzey <mbcacciano@gmail.com>
2018-09-04 13:48:18 -0400

    final assessment Part 2 Q1

---
+++ FinalAssessmentQ1/FinalAssessmentQ1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using FinalAssessmentQ1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalAssessmentQ1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Result(AddNumbersModel model)
+        {
+            ViewBag.Result = model.NumberOne + model.NumberTwo;
+            if (model.NumberOne <= 0 || model.NumberOne > 1000 || model.NumberOne.Equals(null))
+            {
+                ModelState.AddModelError("NumberOne", "Number must be between 1 and 100");
+            }
+            if (model.NumberTwo <= 0 || model.NumberTwo > 1000 || model.NumberTwo.Equals(null))
+            {
+                ModelState.AddModelError("NumberTwo", "Number must be between 1 and 100");
+            }
+
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult About(AddNumbersModel model)
+        {
+            return RedirectToAction("Result");
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FinalAssessmentQ1/FinalAssessmentQ1/Models/AddNumbersModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FinalAssessmentQ1.Models
+{
+    public class AddNumbersModel
+    {
+        [Required]
+        [RegularExpression(@"\d+")]
+        [Range(1, 1000, ErrorMessage = "Sorry but the number must be between 1 and 1000")]
+        public int NumberOne { get; set; }
+        [Required]
+        [RegularExpression(@"\d+")]
+        [Range(1, 1000, ErrorMessage = "Sorry but the number must be between 1 and 1000")]
+        public int NumberTwo { get; set; }
+    }
+}
